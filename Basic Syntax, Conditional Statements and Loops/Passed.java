4e77c04c358b3c067b01fe5256b12ccd665ccbba
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-07-13 18:12:58 +0300

    Create Passed.java

---
+++ Basic Syntax, Conditional Statements and Loops/Passed.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if(grade >= 3.00) {
+            System.out.println("Passed!");
+        }else {
+            return;
+        }
+
+
+    };
+};
+
+
+->  4
+    Passed!
